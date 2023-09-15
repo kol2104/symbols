a53617fe058bc0a360835b4905c2bd12409a207a
kol2104 <kni2104kni@mail.ru>
2023-09-15 23:02:17 +0500

    Init

---
+++ src/main/java/ru/t1consulting/symbols/SymbolsApplication.java
@@ -0,0 +1,13 @@
+package ru.t1consulting.symbols;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymbolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymbolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/t1consulting/symbols/SymbolsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.t1consulting.symbols;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymbolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
